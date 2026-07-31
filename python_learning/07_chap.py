952afd7995befa7c27b7565548ffd387571a399e
Mujjamil-kherada <mujjamilkherada@gmail.com>
2026-07-31 16:12:22 +0530

    fix submodule issue

---
+++ python_learning/07_chap.py
@@ -0,0 +1,184 @@
+# while loop--------------------------------------------------------------------------
+
+# number= 1
+# while number < 5:
+#     print(number)
+#     number=number + 1
+
+# count=1
+# while count < 4:
+#     print(count)
+#     count=count+1
+
+# number=1
+# while number <10:
+#     if number % 2==0:
+#         print(number)
+#     number=number + 1
+
+# number=1
+# while number <10:
+#     if number % 2==0:
+#         print(number)
+#     number=number + 1
+
+
+# number=1
+# while number <20:
+#     if number % 3==0:
+#         print(number)
+#     number=number + 1
+
+
+# number=1
+# while number < 50:
+#     if number % 2 ==0:
+#         print(number)
+#     number=number + 1
+
+# nu
+
+# number= 1
+# while True:
+#     print(number)
+#     number=number + 1
+#     if number > 10 :
+#         break
+
+# number=1
+# while number < 100:
+#     if number %2==1:
+#         print(number)
+#     number=number + 1
+
+# number=int(input('enter tha number:'))
+# while number >0:
+#     print('give number:',number)
+# number-=1
+
+# number=1
+# while number < 30:
+#     if number % 3==0:
+#         print (number)
+#     number=number + 1
+
+# total=0
+# while True:
+#     number=int(input('enter number:'))
+#     number_1=int(input('enter number:'))
+#     if number < 0:
+#         break
+#     total = total + number + number_1
+#     print('total number:',total)
+
+
+# number = 10
+# while True:
+#     print(number)
+#     number=number -1
+#     if number < 1:
+#         break
+
+# number=20
+# while True:
+#     print(number)
+#     number=number -1
+#     if number < 1:
+#        break
+
+# number=1
+# while number < 5:
+#     print(number)
+#     number=number + 1
+
+# a=1
+# while a < 5:
+#     print('hello')
+#     a=a+1
+
+# num=1
+# sum = 0
+# while num < 10:
+#     sum=sum+num
+#     num=num+1
+#     print('sum=',sum)
+
+
+# num=1
+# while  num < 50:
+#     if num % 5==0:
+#         print(num)
+#         num=num+1
+
+# name=(input('enter your name:'))
+# a=1
+# while a <5:
+#     print('hello',name)
+#     a = a + 1
+
+
+# for loop -----------------------------------------------------------------------------
+
+# for a in range(1,11):
+#     print(a)
+
+
+# fruit = ['APPLE','BANANA','MANGO']
+# for a in fruit :
+#     print(a)
+
+# total=0
+# for a in range (1,11):
+#     total += a
+# print('sum:',total)
+
+
+# num=6
+# while num > 1:
+#     print(num)
+#     num-=1
+
+# for a in range(1,11):
+#     print(2,'x',a,2*a)
+
+# for a in range(1,11):
+#     if a % 2==0:
+#         print(a)
+
+# for a in range(1,8):
+#     print('*'* a)
+
+# for a in range(10,3,-1):
+#     print(a)
+
+
+
+# for a in range(10,0,-1):
+#     print(a)
+
+
+# for a in range(1,11):
+#     if a % 2 ==0:
+#         print(a)
+
+# for a in range(5):
+#     for b in range(5):
+#         print('m',end='')
+#     print()
+
+# for a in range(1,11):
+#     print('2','x',2*a)
+
+
+
+# n=5
+# for i in range(1,n+1):
+#     print(" "*(n-i)+"* " * i)
+
+# n=11
+# num=65
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(chr(num),end='')
+#         num+=1
+#     print()
---
+++ python_learning/my_game.py
@@ -0,0 +1,161 @@
+# import rendom
+# come=('computer choos a : rok (r),papaer(p),kater(k)')
+# come=(rendom,rendint(1,3))
+# if come=1:
+#     come='r'
+# if come=2:
+#     come='p'
+# if come=3:
+#     come='k'
+# you=input('enter you choos: rok (r),papaer(p),kater(k)')
+
+# def woh_wine(come,you):
+#     if come=you:
+#         return None
+
+#     elif come='r':
+#         if you='k':
+#             return True
+
+#     elif come='k':
+#         if you='p':
+#             return True
+            
+#     elif come='p':
+#         if you='r':
+#             return True
+
+#     elif come='r':
+#         if you='p':
+#             return False
+    
+#      elif come='p':
+#         if you='k':
+#             return False
+            
+#      elif come='k':
+#         if you='r':
+#             return False
+
+# result=woh_wine(come,you)
+# if result:
+#     print(f'you are wine{come},good job')
+# elif rejult is None :
+#     print(f'mache is{come},drew trye agen')
+# else:
+#     print(f'you are loos ths {come},game trye agen')
+
+
+# import rendom
+# com=('computer choos a : rok (r),papaer(p),kater(k)')
+# com=(rendom,rendint(1,3))
+# if com==1:
+#     com='r'
+# if com==2:
+#     com='p'
+# if com==3:
+#     com='k'
+# you=input('you choos a : rok (r),papaer(p),kater(k)')
+# def woh_wine(com,you):
+#     if com=you:
+#         return None 
+    
+#     elif come='r':
+#         if you='k':
+#             return True
+
+#     elif come='k':
+#         if you='p':
+#             return True
+
+#     elif come='p':
+#         if you='r':
+#             return True
+             
+#     elif come='r':
+#         if you='p':
+#             return False
+    
+#     elif come='p':
+#         if you='k':
+#             return False
+
+#     elif come='k':
+#         if you='r':
+#             return False
+
+# rejult(woh_wine(com,you))
+# if rejult:
+#     print(f'wine{com}')
+# elif rejult is None:
+#     print(f'drew{com}')
+#     print(f'loos{com}')
+#     print(f'loos{com}')
+# else:
+#     print(f'loos{com}')
+#     print(f'loos{com}')
+#     print(f'loos{com}')
+
+
+# class Train:
+#     def __init__(self,name,fare,seat):
+#         self.name=name
+#         self.fare=fare
+#         self.seat=seat
+#     def get_status(self):
+#         print(f"The Train name is :{self.name}")
+#         print(f"Avilailble Seats Are :{self.seat}")
+#         print(f"*"*30)
+
+# vande_bhart=Train(name="Vande Bharat",fare=200,seat=10)
+# vande_bhart.get_status()
+
+
+# class Train:
+#     def __init__(self,name,fare,seat):
+#         self.name=name
+#         self.fare=fare
+#         self.seat=seat
+#     def get_status(self):
+#         print(f"The Train name is :{self.name}")
+#         print(f"Avilailble Seats Are :{self.seat}")
+#         print(f"*"*30)
+#     def get_fare(self):
+#         print(f"Ticket Price RS.{self.fare}")
+
+#     def Book_ticket(self):
+#         if self.seat>0:
+#             print(f"Thanks your seat is Booked, Seat Number{self.seat}")
+#             self.seat-=1
+#         else:
+#             print("Sorry ... Train are fully booked")
+#             print(f"*"*30)
+#     def cancel_Ticket(self):
+#         self.seat += 1
+#         print("Ticket was canceled")
+
+
+#     def gr_coach(self):
+#         print("genral coach is available")
+
+#     def ac_coach(self):
+#         print("AC coach is available")
+    
+#     def is_food(self):
+#         print("Food is available in the tarin")
+#         print(f"*"*30)
+
+
+# vande_bhart=Train(name="Vande Bharat",fare=200,seat=10)
+# vande_bhart.get_status()
+# vande_bhart.get_fare()
+# vande_bhart.Book_ticket()
+# vande_bhart.get_status()
+# vande_bhart.gr_coach()
+# vande_bhart.ac_coach()
+# vande_bhart.is_food()
+# vande_bhart.cancel_Ticket()
+
+
+# a=['apple','banana']
+# b=a.
